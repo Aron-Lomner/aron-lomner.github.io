a0ad66a8df4acfb512ccd82ee93753716f1d660f
unstack-underflow <aronlomn@gmail.com>
2023-12-05 21:21:06 -0500

    add image padding and changed size and scale

--- js/main.js
+++ js/main.js
@@ -1,38 +1,44 @@
-import { typeWrighter, sleep } from './utils.js';
-import {Particles} from './particles.js';
+import { typeWrighter, sleep } from "./utils.js";
+import { Particles } from "./particles.js";
 
-async function initialize(){
-    let greeting = document.getElementById('greeting');
-    let pause = typeWrighter(greeting, 'Hello, I\'m');
-    await sleep(pause);
-    let name = document.getElementById('name');
-    pause = typeWrighter(name, 'Aron Lomner');
-    await sleep(pause);
-    let occupation = document.getElementById('occupation')
-    typeWrighter(occupation, 'Fullstack Developer');
+async function initialize() {
+  let greeting = document.getElementById("greeting");
+  let pause = typeWrighter(greeting, "Hello, I'm");
+  await sleep(pause);
+  let name = document.getElementById("name");
+  pause = typeWrighter(name, "Aron Lomner");
+  await sleep(pause);
+  let occupation = document.getElementById("occupation");
+  typeWrighter(occupation, "Fullstack Developer");
 }
 
 initialize();
 
-let container = document.getElementById('logo');
+let container = document.getElementById("logo");
 let settings = {
-    isText: false,
-    color: '#39FF14',
-    font: '18px Verdana',
-    text: 'Welcome!',
-    imagePath: '/assets/images/logo.png',
-    imageWidth: 231,
-    imageHeight: 58,
-    mouseRadius: 15,
-    particleRadius: 1,
-    scale: 1,
-    container: container,
-    weight: 15,
+  isText: false,
+  color: "#39FF14",
+  font: "18px Verdana",
+  text: "Welcome!",
+  imagePath: "/assets/images/logo.png",
+  imageWidth: 231 - 23.1 * 2,
+  imageHeight: 58 - 5.8 * 2,
+  mouseRadius: 15,
+  particleRadius: 1,
+  scale: 1 + 0.1 * 2,
+  container: container,
+  weight: 15,
+  padding: {
+    left: 10,
+    right: 10,
+    top: 10,
+    bottom: 10,
+  },
 };
 
 if (window.innerWidth < 600) {
-    settings.imageHeight = settings.imageHeight / 1.2;
-    settings.imageWidth = settings.imageWidth / 1.2;
+  settings.imageHeight = settings.imageHeight / 1.2;
+  settings.imageWidth = settings.imageWidth / 1.2;
 }
 
 new Particles(settings);
